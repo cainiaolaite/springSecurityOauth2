b2083181ee5ad3c9b8d17cfd732f783db6ccac7a
cainiaolaite <1308189967@qq.com>
2019-03-22 20:11:32 +0800

    增加了 spring session session存储，但是发生了错误
    Signed-off-by: cainiaolaite <1308189967@qq.com>

--- fileplat-cloud/src/main/java/com/hua/fileplat/cloud/base/LoginHandlerInterceptor.java
+++ fileplat-cloud/src/main/java/com/hua/fileplat/cloud/base/LoginHandlerInterceptor.java
@@ -1,6 +1,14 @@
 package com.hua.fileplat.cloud.base;
 
+import com.hua.fileplat.cloud.base.cookie.CookieService;
 import com.hua.fileplat.cloud.user.vo.SessionUserVo;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.web.http.HttpSessionStrategy;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,18 +19,30 @@ import javax.servlet.http.HttpServletResponse;
  * 登录拦截
  */
 public class LoginHandlerInterceptor  implements HandlerInterceptor {
+    private static Logger logger= LoggerFactory.getLogger(LoginHandlerInterceptor.class);
+    @Autowired
+    private CookieService cookieService;
+    @Autowired
+    private SessionRepository sessionRepository;
+    @Autowired
+    private HttpSessionStrategy httpSessionStrategy;
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        SessionUserVo sessionUserVo=(SessionUserVo)httpServletRequest.getSession().getAttribute(SessionUserVo.SESSION_USER_VO);
-        if(httpServletRequest.getUserPrincipal() != null){
-            return true;
+        cookieService.addCookie(httpServletRequest,httpServletResponse);
+        String sessionId=httpSessionStrategy.getRequestedSessionId(httpServletRequest);
+        if(StringUtils.isNotEmpty(sessionId)){
+            Session session=sessionRepository.getSession(sessionId);
+            if(session != null){
+                SessionUserVo sessionUserVo=(SessionUserVo)session.getAttribute(SessionUserVo.SESSION_USER_VO);
+                if(sessionUserVo != null){
+                    return true;
+                }
+            }
         }
-        if(sessionUserVo == null){
-            httpServletResponse.addHeader("sessionstatus", "timeOut");
-            httpServletResponse.sendRedirect("/");
-            return false;
-        }
-        return true;
+        logger.info("sessionId {} 沒有回話",sessionId);
+        httpServletResponse.addHeader("sessionstatus", "timeOut");
+        httpServletResponse.sendRedirect("/");
+        return false;
     }
 
     @Override
---
+++ fileplat-cloud/src/main/java/com/hua/fileplat/cloud/base/ehcache/AopEhcacheService.java
@@ -0,0 +1,16 @@
+package com.hua.fileplat.cloud.base.ehcache;
+
+public class AopEhcacheService {
+
+    public Object getCacheObject(String key){
+        return null;
+    }
+
+    public void putCacheObject(String key,Object object){
+
+    }
+
+    public void removeCacheObject(String key){
+
+    }
+}
