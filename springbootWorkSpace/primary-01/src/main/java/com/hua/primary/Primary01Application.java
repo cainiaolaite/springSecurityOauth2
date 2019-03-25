1828bb93911af332299cab7ae1698a2448d6f0e7
cainiaolaite <1308189967@qq.com>
2019-03-25 21:53:40 +0800

    spring boot 开发demo 涵盖内容 1知识点介绍  2错误页面处理（404,500） 3多环境部署
    Signed-off-by: cainiaolaite <1308189967@qq.com>

---
+++ springbootWorkSpace/primary-01/src/main/java/com/hua/primary/Primary01Application.java
@@ -0,0 +1,13 @@
+package com.hua.primary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Primary01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Primary01Application.class, args);
+	}
+
+}
